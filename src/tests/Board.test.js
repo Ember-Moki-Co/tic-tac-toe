08a2a8f34197887148b7ee697e3f56d56bb6d5c7
Renzlawr <RSLogan23@gmail.com>
2022-11-17 15:14:18 -0700

    linter

--- src/tests/Board.test.js
+++ src/tests/Board.test.js
@@ -1,20 +1,20 @@
-import Board from "../components/Board.jsx"
-import { render, screen } from "@testing-library/react"
+import Board from '../components/Board.jsx'
+import { render, screen } from '@testing-library/react'
 
 describe('Board', () => {
   beforeEach(() => {
     render(
       <Board />
-    ) 
+    )
   })
-  
+
   it('should render the board correctly', () => {
-    const gameBoard = screen.getByTestId("game-board")
+    const gameBoard = screen.getByTestId('game-board')
     expect(gameBoard).toBeInTheDocument
   })
-  
+
   it('should render all 9 tiles of the board correctly', () => {
-    const gameTile = screen.getAllByTestId("game-tile")
+    const gameTile = screen.getAllByTestId('game-tile')
     expect(gameTile).toHaveLength(9)
   })
 })
