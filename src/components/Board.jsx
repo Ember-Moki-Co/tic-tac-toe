3020a6d2a7b2ccc08f36c4700f12080be12f4ac4
Renzlawr <RSLogan23@gmail.com>
2022-11-17 17:40:07 -0700

    added clicking and switching logic
    Co-authored-by: Sahtra <SahtraRG@users.noreply.github.com>

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Tile from './Tile'
 import '../stylesheets/Board.scss'
 
 const Board = () => {
-  const board = [null, null, null, null, null, null, null, null, null]
+  const [board, setBoard] = useState([...Array(9).keys()])
+  const [player1, setPlayer1] = useState('❌')
+
   return (
-    <div id="board" data-testid='game-board'>
-      {
-        board.map((tile, index) => {
-          return <Tile key={index} />
-        })
-      }
+    <div id='board' data-testid='game-board'>
+      {board.map((tile, index) => {
+        return (
+          <Tile key={index} tile={tile} board={board} player1={player1} setPlayer1={setPlayer1} />
+        )
+      })}
     </div>
   )
 }
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../stylesheets/Tile.scss'
 
-const Tile = () => {
-  return (
-    <div className="tile" data-testid='game-tile'>
+const Tile = ({ tile, board, player1, setPlayer1 }) => {
+  const handleTileChange = () => {
+    if (typeof board[tile] === 'number') {
+      player1 === '❌' ? setPlayer1('⭕️') : setPlayer1('❌')
+      board[tile] = player1
+    }
+  }
 
+  return (
+    <div
+      className='tile'
+      data-testid='game-tile'
+      onClick={() => handleTileChange()}
+    >
+      {tile}
     </div>
   )
 }
