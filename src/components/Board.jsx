e80ba8a3efb9e350d1c1cb89f5a9be44c64c429b
SahtraRG <sahtra01@gmail.com>
2022-12-18 15:51:07 -0700

    refactor win logic to account for game draw

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,54 +3,45 @@ import Tile from './Tile'
 import '../stylesheets/Board.scss'
 
 const Board = ({ turnOne, turnTwo }) => {
-  const [board] = useState([...Array(9).keys()])
+  const [board, setBoard] = useState([...Array(9).keys()])
   const [currentPlayer, setCurrentPlayer] = useState(turnOne)
-  const [playerOneMoves, setPlayerOneMoves] = useState([])
-  const [playerTwoMoves, setPlayerTwoMoves] = useState([])
-  const [gameWon, setGameWon] = useState(false)
-  const winCondition = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-  let winnerMessage = ' '
+  const [gameOver, setGameOver] = useState(false)
+  const [winnerMessage, setWinnerMessage] = useState(undefined)
 
-  // const draw = playerOneWin === false && playerTwoWin === false
-  // console.log(board.every(tile => typeof tile !== 'number'))
-  // console.log(playerOneWin)
-  const draw = gameWon === false && playerOneMoves.length + playerTwoMoves.length === 9
+  const checkGameOver = (board) => {
+    // Define winning combinations
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
 
-  winCondition.map(conditions => {
-    console.log(board.every(tile => typeof tile !== 'number') && conditions.some(tile => playerOneMoves.includes(tile)))
-    console.log(conditions)
-    // console.log(board.every(tile => typeof tile !== 'number') && conditions.some(tile => playerOneMoves.includes(tile)) === false && conditions.every(tile => playerTwoMoves.includes(tile)) === false)
-    // if (bruh && board.every(tile => typeof tile !== 'number')) {
-    //   winnerMessage = 'Draw!'
-    // }
-    // if (conditions.every(tile => playerOneMoves.includes(!tile)) && conditions.every(tile => playerTwoMoves.includes(!tile))) {
-
-    if (board.every(tile => typeof tile !== 'number') && conditions.some(tile => playerOneMoves.includes(tile)) === false && conditions.some(tile => playerTwoMoves.includes(tile)) === false) {
-      winnerMessage = 'Draw!'
-    } else if ((conditions.every(tile => playerOneMoves.includes(tile)) && playerOneMoves.length >= 3) || (board.every(tile => typeof tile !== 'number') && conditions.every(tile => playerOneMoves.includes(tile)))) {
-      () => setGameWon(true)
-      winnerMessage = `${turnOne} wins!`
-    } else if ((conditions.every(tile => playerTwoMoves.includes(tile)) && playerTwoMoves.length >= 3) || (board.every(tile => typeof tile !== 'number') && conditions.every(tile => playerTwoMoves.includes(tile)))) {
-      () => setGameWon(true)
-      winnerMessage = `${turnTwo} wins!`
-    } else if (board.every(tile => typeof tile !== 'number')) {
-      winnerMessage = 'Draw!'
+    const wins = winningCombos.map((combo) =>
+      combo.every((i) => typeof board[i] !== 'number' && board[i] === board[combo[0]])
+    )
+    if (wins.includes(true)) {
+      setGameOver(true)
+      setWinnerMessage(`${currentPlayer} wins!`)
+      return
     }
-    return undefined
-  })
 
-  // else if (board.every(tile => typeof tile !== 'number') && playerOneMoves.length + playerTwoMoves.length === 9 && draw) {
-  //   winnerMessage = 'Draw!'
-  // }
+    if (board.every((square) => typeof square !== 'number')) {
+      setGameOver(true)
+      setWinnerMessage('Draw!')
+    }
+  }
+
+  const resetGame = () => {
+    setBoard([...Array(9).keys()])
+    setCurrentPlayer(turnOne)
+    setGameOver(false)
+  }
+
   return (
     <>
       <h1>{winnerMessage}</h1>
@@ -63,11 +54,11 @@ const Board = ({ turnOne, turnTwo }) => {
               board={board}
               currentPlayer={currentPlayer}
               setCurrentPlayer={setCurrentPlayer}
-              setPlayerOneMoves={setPlayerOneMoves}
-              setPlayerTwoMoves={setPlayerTwoMoves}
-              winnerMessage={winnerMessage}
               turnOne={turnOne}
               turnTwo={turnTwo}
+              gameOver={gameOver}
+              setBoard={setBoard}
+              checkGameOver={checkGameOver}
             />
           )
         })}
